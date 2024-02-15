7bf9dbdc168006c78568ad335cb1aa494f87b24d
SilverCircuits <121541637+c-a-parr@users.noreply.github.com>
2024-02-14 20:25:28 -0600

    Added an image.

--- TeamPanel.java
+++ TeamPanel.java
@@ -11,23 +11,25 @@ public class TeamPanel extends JPanel {
         memberLabels = new JLabel[TEAM_SIZE];
 
         // create your own label that have an image and your name as text.
-        memberLabels[0] = new JLabel("Connor Sample", new ImageIcon("smile.png"), SwingConstants.LEFT);
+        memberLabels[0] = new JLabel("Malenia, Blade of Miquella", new ImageIcon("Malenia.jpeg"), SwingConstants.LEFT);
         memberLabels[1] = new JLabel("Dakotah Sample", new ImageIcon("funny-animal-picture-hnczwmybpoojbyse.jpg"), SwingConstants.CENTER);
         memberLabels[2] = new JLabel("Ethan Sample", new ImageIcon("TonyHappy.jpg"), SwingConstants.RIGHT);
-        memberLabels[0].setHorizontalTextPosition(SwingConstants.LEFT);
+        memberLabels[0].setHorizontalTextPosition(SwingConstants.CENTER);
         memberLabels[0].setVerticalTextPosition(SwingConstants.BOTTOM);
-        memberLabels[1].setHorizontalTextPosition(SwingConstants.CENTER);
-        memberLabels[1].setVerticalTextPosition(SwingConstants.BOTTOM);
-        memberLabels[2].setHorizontalTextPosition(SwingConstants.RIGHT);
-        memberLabels[2].setVerticalTextPosition(SwingConstants.BOTTOM);
+        memberLabels[1] = new JLabel();
+        memberLabels[2] = new JLabel();
 
-
+        /*
+        memberLabels[0] = new JLabel("Malenia, Blade of Miquella", new ImageIcon("Malenia.jpeg"), SwingConstants.CENTER);
+        memberLabels[0].setHorizontalTextPosition(SwingConstants.CENTER);
+        memberLabels[0].setVerticalTextPosition(SwingConstants.BOTTOM);
+        */
 
         for (JLabel member : memberLabels) {
             this.add(member);
         }
 
-        this.setPreferredSize(new Dimension(1920, 1080));
+        this.setPreferredSize(new Dimension(500, 250));
         this.setBackground(Color.LIGHT_GRAY);
 
 
